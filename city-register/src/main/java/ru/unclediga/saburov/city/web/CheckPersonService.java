6d86a7b20a3a98cdaf31d5fcbe458efbe3606ec9
homeuser <zowie_i@mail.ru>
2020-10-16 13:38:16 +0300

    city-register: (104/1) REST @PathParam, @QueryParam

--- city-register/src/main/java/ru/unclediga/saburov/city/web/CheckPersonService.java
+++ city-register/src/main/java/ru/unclediga/saburov/city/web/CheckPersonService.java
@@ -2,11 +2,21 @@ package ru.unclediga.saburov.city.web;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
 @Path("/check")
 public class CheckPersonService {
     @GET
     public String checkPerson(){
         return "Test String";
+    }
+
+    @GET
+    @Path("{id}")
+    // localhost:8080/city/rest/check/101?name=SimpleName
+    public String checkWithParam(@PathParam("id") int simpleId,
+                                 @QueryParam("name") String simpleName) {
+        return "params:" + simpleId + ":" + simpleName;
     }
 }
