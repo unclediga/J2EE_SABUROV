3b89e3c38314b343bd4d380312283cde535bbd8f
homeuser <zowie_i@mail.ru>
2020-10-19 18:47:01 +0300

    register-office: (111) simple Hibernate session (java config)

--- register-office/src/main/java/ru/unclediga/saburov/register/manager/PersonManager.java
+++ register-office/src/main/java/ru/unclediga/saburov/register/manager/PersonManager.java
@@ -8,13 +8,12 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.query.Query;
 import ru.unclediga.saburov.register.domain.Person;
 
-import java.io.Serializable;
 import java.util.List;
 
 public class PersonManager {
     public static void main(String[] args) {
 
-        final SessionFactory sessionFactory = getSessionFactory();
+        final SessionFactory sessionFactory = getSessionFactoryNoFile();
         Session session = sessionFactory.openSession();
         session.getTransaction().begin();
         Person person = new Person();
@@ -44,6 +43,43 @@ public class PersonManager {
         final StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
         final StandardServiceRegistry serviceRegistry = serviceRegistryBuilder.configure("hibernate.cfg.xml").build();
         final MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+        return metadataSources.buildMetadata().buildSessionFactory();
+    }
+
+    private static SessionFactory getSessionFactoryNoFile() {
+        final StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
+        /*
+         <hibernate-configuration>
+            <session-factory>
+                <property name="hibernate.connection.url">jdbc:mysql://localhost:3306/db_example</property>
+                <property name="hibernate.connection.driver_class">com.mysql.cj.jdbc.Driver</property>
+                <property name="hibernate.connection.username">springuser</property>
+                <property name="hibernate.connection.password">springuser</property>
+                <property name="hibernate.dialect">org.hibernate.dialect.MySQL8Dialect</property>
+                <property name="hibernate.show_sql">true</property>
+                ...
+            </session-factory>
+        </hibernate-configuration>
+         */
+        serviceRegistryBuilder.applySetting("hibernate.connection.url", "jdbc:mysql://localhost:3306/db_example");
+        serviceRegistryBuilder.applySetting("hibernate.connection.driver_class","com.mysql.cj.jdbc.Driver");
+        serviceRegistryBuilder.applySetting("hibernate.connection.username","springuser");
+        serviceRegistryBuilder.applySetting("hibernate.connection.password","springuser");
+        serviceRegistryBuilder.applySetting("hibernate.dialect","org.hibernate.dialect.MySQL8Dialect");
+        serviceRegistryBuilder.applySetting("hibernate.show_sql","true");
+
+        final StandardServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
+        final MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+        /*
+         <hibernate-configuration>
+            <session-factory>
+                ...
+                <mapping class="ru.unclediga.saburov.register.domain.Person" />
+            </session-factory>
+        </hibernate-configuration>
+         */
+        metadataSources.addAnnotatedClass(Person.class);
+
         return metadataSources.buildMetadata().buildSessionFactory();
     }
 }
