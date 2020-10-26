d456f8e968290f3e76d7772394931949ada32f4e
homeuser <zowie_i@mail.ru>
2020-10-26 23:11:39 +0300

    register-office: (122/2) Spring Framework: + java config

---
+++ register-office/src/main/java/ru/unclediga/saburov/register/config/MarriageConfig.java
@@ -0,0 +1,17 @@
+package ru.unclediga.saburov.register.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.unclediga.saburov.register.dao.PersonDao;
+
+@Configuration
+public class MarriageConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageConfig.class);
+    @Bean
+    public PersonDao buildPersonDao(){
+        LOGGER.info("PersonDao is being created...");
+        return new PersonDao();
+    }
+}
--- register-office/src/main/java/ru/unclediga/saburov/register/manager/MarriageManager.java
+++ register-office/src/main/java/ru/unclediga/saburov/register/manager/MarriageManager.java
@@ -3,17 +3,21 @@ package ru.unclediga.saburov.register.manager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import ru.unclediga.saburov.register.dao.MarriageDao;
+import ru.unclediga.saburov.register.dao.PersonDao;
 import ru.unclediga.saburov.register.domain.MarriageCertificate;
 import ru.unclediga.saburov.register.view.MarriageRequest;
 import ru.unclediga.saburov.register.view.MarriageResponse;
 
-@Component
+@Service
 public class MarriageManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(MarriageManager.class);
     @Autowired
     private MarriageDao marriageDao;
+
+    @Autowired
+    private PersonDao personDao;
 
     public void setMarriageDao(MarriageDao marriageDao) {
         this.marriageDao = marriageDao;
