23a5fe40590a20df58c8bb6bd8a23f1df89cafeb
homeuser <zowie_i@mail.ru>
2020-10-28 13:13:37 +0300

    register-office: (125) Spring Data: JPA Repository

--- register-office/src/main/java/ru/unclediga/saburov/register/dao/MarriageDao.java
+++ register-office/src/main/java/ru/unclediga/saburov/register/dao/MarriageDao.java
@@ -1,25 +1,9 @@
 package ru.unclediga.saburov.register.dao;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.unclediga.saburov.register.domain.MarriageCertificate;
-import ru.unclediga.saburov.register.view.MarriageRequest;
 
-@Component
-public class MarriageDao {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageDao.class);
-    @Value("${test.value}")
-    private String test;
-
-    public void setTest(String test) {
-        this.test = test;
-    }
-
-    public MarriageCertificate findMarriageCertificate(MarriageRequest request) {
-        LOGGER.info("findMarriageCertificate called:{}", test);
-        return null;
-    }
-
+@Repository
+public interface MarriageDao extends JpaRepository<MarriageCertificate, Long> {
 }
--- register-office/src/main/java/ru/unclediga/saburov/register/manager/MarriageManager.java
+++ register-office/src/main/java/ru/unclediga/saburov/register/manager/MarriageManager.java
@@ -10,6 +10,7 @@ import ru.unclediga.saburov.register.dao.MarriageDao;
 import ru.unclediga.saburov.register.dao.PersonDao;
 import ru.unclediga.saburov.register.domain.MarriageCertificate;
 import ru.unclediga.saburov.register.domain.Person;
+import ru.unclediga.saburov.register.domain.PersonFemale;
 import ru.unclediga.saburov.register.domain.PersonMale;
 import ru.unclediga.saburov.register.view.MarriageRequest;
 import ru.unclediga.saburov.register.view.MarriageResponse;
@@ -32,21 +33,45 @@ public class MarriageManager {
 
     public MarriageResponse findMarriageCertificate(MarriageRequest request) {
         LOGGER.info("findMarriageCertificate called");
-        final MarriageCertificate marriageCertificate = marriageDao.findMarriageCertificate(request);
         personDao.findPersons();
 
-        personDao.addPerson(getPerson());
-        personDao.addPerson(getPerson());
+        Long wifeId = personDao.addPerson(getPerson(1));
+        final PersonFemale wife = (PersonFemale) personDao.findById(wifeId);
+        Long husbandId = personDao.addPerson(getPerson(2));
+        final PersonMale husband = (PersonMale) personDao.findById(husbandId);
+
+
+        MarriageCertificate mc = getMarriageCertificate(wife, husband);
+        marriageDao.saveAndFlush(mc);
+        LOGGER.info("findMarriageCertificateId( id => " + mc.getMarriageCertificateId() + ")");
+        marriageDao.findById(mc.getMarriageCertificateId());
 
         return new MarriageResponse();
     }
 
-    private Person getPerson() {
-        final Person person = new PersonMale();
-        person.setLastName("L-name");
-        person.setFirstName("F-name");
+    private MarriageCertificate getMarriageCertificate(PersonFemale wife, PersonMale husband) {
+        final MarriageCertificate mc = new MarriageCertificate();
+        mc.setIssueDate(LocalDate.now());
+        mc.setNumber("1234");
+        mc.setActive(true);
+        mc.setHusband(husband);
+        mc.setWife(wife);
+        return mc;
+    }
+
+    private Person getPerson(int sex) {
+        final Person person;
+        if (sex == 1) {
+            person = new PersonFemale();
+            person.setLastName("L-female name ");
+            person.setFirstName("F-female name");
+        } else {
+            person = new PersonMale();
+            person.setLastName("L-male name ");
+            person.setFirstName("F-male name");
+        }
         person.setPatronymic("Patronymic");
-        person.setDateOfBirth(LocalDate.of(1991,3,12));
+        person.setDateOfBirth(LocalDate.of(1991, 3, 12));
         return person;
     }
 }
