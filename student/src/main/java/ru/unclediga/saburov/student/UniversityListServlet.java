e6d49e8fbf4da2f78cf3fa9692c98a06c1e1062b
homeuser <zowie_i@mail.ru>
2020-11-03 12:17:51 +0300

    student: 135 Simple JSP-page

---
+++ student/src/main/java/ru/unclediga/saburov/student/UniversityListServlet.java
@@ -0,0 +1,16 @@
+package ru.unclediga.saburov.student;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UniversityListServlet", urlPatterns = "/universityList")
+public class UniversityListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/universityList.jsp").forward(req, resp);
+    }
+}
---
+++ student/src/main/webapp/universityList.jsp
@@ -0,0 +1,10 @@
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Unuversity List</title>
+</head>
+<body>
+<H2>Hello from JSP!</H2>
+</body>
+</html>
