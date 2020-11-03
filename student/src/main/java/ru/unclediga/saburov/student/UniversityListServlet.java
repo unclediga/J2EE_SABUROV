cead3f5e8f4c1c9b30a8daeef6fed5e409182449
homeuser <zowie_i@mail.ru>
2020-11-03 14:55:07 +0300

    student: 138 What if to define servlet as a spring bean?

--- student/src/main/java/ru/unclediga/saburov/student/UniversityListServlet.java
+++ student/src/main/java/ru/unclediga/saburov/student/UniversityListServlet.java
@@ -1,5 +1,7 @@
 package ru.unclediga.saburov.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 import ru.unclediga.saburov.student.domain.University;
@@ -13,8 +15,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+/* What if to define servlet as a spring bean?
+   for testing only ;-)
+  will be created two DIFFERENT classes of UniversityListServlet.class:
+   1nd - servlet for Tomcat
+   2nd - bean for Spring    */
+@Service
 @WebServlet(name = "UniversityListServlet", urlPatterns = "/universityList")
 public class UniversityListServlet extends HttpServlet {
+    @Autowired
+    private UniversityService uservice;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
@@ -22,6 +33,36 @@ public class UniversityListServlet extends HttpServlet {
         final List<University> list = universityService.findUniversity();
         list.forEach(u -> System.out.println(u.getUniversityId() + ":" + u.getUniversityName()));
 
+
+        // NullPointerException
+        // servlet is not Spring-bean and can't have @Autowired fields
+        try {
+            uservice.findUniversity();
+        } catch (NullPointerException e) {
+            System.out.println("!!!NullPointerException detected for 'uservice' !!!");
+        }
+
+        // but servlet class, used as Spring-bean, surely have @Autowired field
+        final UniversityListServlet universityListServlet = context.getBean(UniversityListServlet.class);
+        final List<University> list2 = universityListServlet.uservice.findUniversity();
+        System.out.println("University List from Servlet/Bean size = " + list2.size());
+
+        System.out.println("Servlet Tomcat hashcode:" + this.hashCode());
+        System.out.println("Servlet Spring hashcode:" + universityListServlet.hashCode());
+
+        /* System out:
+         ----------------------------------------
+         !!!NullPointerException detected for 'uservice' !!!
+            Hibernate:
+                select
+                    university0_.university_id as universi1_3_,
+                    university0_.university_name as universi2_3_
+                from
+                    sr_university university0_
+            University List from Servlet/Bean size = 1
+            Servlet Tomcat hashcode:7559247
+            Servlet Spring hashcode:185422
+         */
         getServletContext().getRequestDispatcher("/universityList.jsp").forward(req, resp);
     }
 }
