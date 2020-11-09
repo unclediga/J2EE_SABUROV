2b6285d242b94e32693cbebdf9811991acc7e241
homeuser <zowie_i@mail.ru>
2020-11-09 13:35:56 +0300

    student: (144/1) Spring MVC: Jersey -> Spring REST

--- student/src/main/java/ru/unclediga/saburov/student/StudentController.java
+++ student/src/main/java/ru/unclediga/saburov/student/StudentController.java
@@ -1,28 +1,29 @@
 package ru.unclediga.saburov.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.unclediga.saburov.student.service.StudentService;
 import ru.unclediga.saburov.student.view.StudentRequest;
 import ru.unclediga.saburov.student.view.StudentResponse;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Component
-@Path("/student")
+@RestController
+@RequestMapping("/student")
 public class StudentController {
     @Autowired
     private StudentService studentService;
 
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
+//    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
     public List<StudentResponse> getStudentInfo(StudentRequest request) {
         return studentService.getStudentInfo(request);
+    }
+
+    @RequestMapping("/check")
+    public String checkAdmin() {
+        return "REST Service is working!";
     }
 }
